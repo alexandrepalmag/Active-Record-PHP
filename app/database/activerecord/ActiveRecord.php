ecd1af607cf92ce31d0b8a04940e785cb6dc3214
alexandrepalmag <alepalmag85ti@gmail.com>
2021-07-26 16:57:00 -0300

    Creation ActiveRecord and User.php.

--- app/database/activerecord/ActiveRecord.php
+++ app/database/activerecord/ActiveRecord.php
@@ -2,3 +2,50 @@
 
 namespace app\database\activerecord;
 
+use Attribute;
+use ReflectionClass;
+
+abstract class ActiveRecord
+{
+
+    protected $table = null;
+
+    protected $attributes = [];
+
+    protected $value;
+
+    public function __construct()
+    {
+
+        if (!$this->table) {
+
+            $this->table = strtolower((new ReflectionClass($this))->getShortName());
+        }
+    }
+
+    public function getTable()
+    {
+
+        return $this->table;
+    }
+
+    public function getAttributes()
+    {
+
+        return $this->attributes;
+    }
+
+
+    public function __set($attributes, $value)
+    {
+
+        $this->attributes[$attributes] = $value;
+    }
+
+    public function __get($attribute)
+    {
+
+        return $this->attributes[$attribute];
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -2,4 +2,12 @@
 
 require '../vendor/autoload.php';
 
+use app\database\models\User;
 
+$user = new User;
+
+$user->firstName = 'Alexandre';
+$user->lastName = 'Goncalves';
+$user->id = 1;
+
+var_dump($user);
